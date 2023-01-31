1c71d9156d70c5dd0f29fcf490670193559369bc
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-01-30 22:20:51 -0600

    Added final functionality to Encounter Creator

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@
  Variables
  ***********************************************************************/
 var encounterSection = document.getElementById("encounter-creator");
-
 var addMonsterBtn = document.getElementById("add-monster-btn");
 var addMonsterInput = document.getElementById("add-monster-input");
 var monsterList = document.getElementById("monster-list");
@@ -17,10 +16,37 @@ var snowMusicBtn = document.getElementById("snow-music-btn");
 var tavernMusicBtn = document.getElementById("tavern-music-btn");
 var rainMusicBtn = document.getElementById("rain-music-btn");
 var scaryMusicBtn = document.getElementById("scary-music-btn");
+var campMusicBtn = document.getElementById("camp-music-btn");
+var bossMusicBtn = document.getElementById("boss-music-btn");
 
 /************************************************************************
  Functions
  ***********************************************************************/
+
+// Function to populate options
+function populateMonsters() {
+  var requestApi = "https://www.dnd5eapi.co/api/monsters";
+  // console.log(requestApi);
+
+  fetch(requestApi)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // console.log(data);
+
+      for (let i = 0; i < data.count; i++) {
+        // console.log(data.results[i].name);
+        var monsterList = document.createElement("option");
+        monsterList.innerHTML = data.results[i].name;
+        monsterList.value = data.results[i].index;
+        addMonsterInput.appendChild(monsterList);
+      }
+    });
+}
+
+//Populate Monsters
+populateMonsters();
 
 function addMonster(event) {
   event.preventDefault();
@@ -36,7 +62,10 @@ function addMonster(event) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      // console.log(data);
+
+      var monsterTableRow = document.createElement("tr");
+      monsterTable.appendChild(monsterTableRow);
 
       var monsterTableHeader = document.createElement("th");
       monsterTableHeader.classList =
@@ -71,13 +100,55 @@ function addMonster(event) {
       monsterHealthTd.classList = "px-6 py-4";
       monsterTable.appendChild(monsterHealthTd);
 
+      // Add Hit Points
       var monsterHealthSpan = document.createElement("span");
       monsterHealthSpan.classList =
         "inline-flex items-center gap-1 rounded-full bg-red-50 px-2 py-1 text-s font-semibold text-red-600";
       monsterHealthSpan.append(data.hit_points);
       monsterHealthTd.appendChild(monsterHealthSpan);
+
+      // Add Monster Armor
+      var monsterArmorTd = document.createElement("td");
+      monsterArmorTd.classList = "px-6 py-4";
+      monsterTable.appendChild(monsterArmorTd);
+
+      // Add Hit Points
+      var monsterArmorSpan = document.createElement("span");
+      monsterArmorSpan.classList =
+        "inline-flex items-center gap-1 rounded-full bg-gray-100 px-2 py-1 text-s font-semibold text-gray-600";
+      monsterArmorSpan.append(data.constitution);
+      monsterArmorTd.appendChild(monsterArmorSpan);
+
+      // Add Button to Delete
+      var monsterBtnTd = document.createElement("td");
+      monsterBtnTd.classList = "px-6 py-4";
+      monsterTable.appendChild(monsterBtnTd);
+
+      var monsterBtnDiv = document.createElement("div");
+      monsterBtnDiv.classList = "flex justify-end gap-4";
+      monsterBtnTd.appendChild(monsterBtnDiv);
+
+      var monsterBtnMain = document.createElement("button");
+      monsterBtnMain.classList =
+        "delete-btn shadow bg-red-400 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded";
+      monsterBtnMain.textContent = "Delete";
+      monsterBtnDiv.appendChild(monsterBtnMain);
     });
 }
+
+// Delete Monster Row
+function deleteBtn(event) {
+  event.preventDefault();
+  if (event.target.matches(".delete-btn")) {
+    console.log("Delete");
+    monsterTable.remove();
+  }
+}
+monsterTable.addEventListener("click", deleteBtn);
+
+/************************************************************************
+Spotify Buttons
+ ***********************************************************************/
 
 //Button Functions to change background image based on spotify choice
 function changeBgSnow(event) {
@@ -100,8 +171,22 @@ function changeBgScary(event) {
   spotifyPlayer.style.backgroundImage = "url(./assets/images/DnD_Scary.png)";
 }
 
+function changeBgCamp(event) {
+  event.preventDefault();
+  spotifyPlayer.style.backgroundImage = "url(./assets/images/DnD_Camp.jpg)";
+}
+
+function changeBgBoss(event) {
+  event.preventDefault();
+  spotifyPlayer.style.backgroundImage = "url(./assets/images/DnD_Boss.jpg)";
+}
+
+// Create Encounter
 function createEncounter() {
   var encounterInfo = document.getElementById("encounter-textarea").value;
+  var encounterInfoSection = document.getElementById("encounter-text");
+
+  encounterInfoSection.innerHTML = encounterInfo;
   console.log(encounterInfo);
 
   var myTable = document.getElementById("monster-table");
@@ -112,15 +197,11 @@ function createEncounter() {
   monsterEncounterTable.innerHTML = myTable.innerHTML;
 
   //Todo: Save Encounter to Local Storage
-
-  // var encounter = {
-  //   encounterTextInput: encounterInfo,
-  // };
 }
 
 /************************************************************************
 Calls
- ***********************************************************************/
+***********************************************************************/
 
 // Add Monster Eventlistener
 addMonsterBtn.addEventListener("click", addMonster);
@@ -133,3 +214,5 @@ snowMusicBtn.addEventListener("click", changeBgSnow);
 tavernMusicBtn.addEventListener("click", changeBgTavern);
 rainMusicBtn.addEventListener("click", changeBgRain);
 scaryMusicBtn.addEventListener("click", changeBgScary);
+campMusicBtn.addEventListener("click", changeBgCamp);
+bossMusicBtn.addEventListener("click", changeBgBoss);
